9a5be3f79cc2d56767a72a8ae2780c35720e1ca9
vaquierm <michaelvaquier74@gmail.com>
2019-12-29 17:21:25 -0500

    ElevatorAI base class made

---
+++ ElevatorSimulator/ElevatorAI/BenchmarkAI.cs
@@ -0,0 +1,25 @@
+﻿using ElevatorSimulator.Elevator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorSimulator.ElevatorAI
+{
+    class BenchmarkAI : ElevatorAI
+    {
+        public BenchmarkAI(Building building, ElevatorCollection elevators) : base(building, elevators)
+        {
+            
+        }
+
+        /// <summary>
+        /// Make decisions with the pending requests and order the elevators to move accordingly
+        /// </summary>
+        public override void HandleRequests()
+        {
+
+        }
+    }
+}
---
+++ ElevatorSimulator/ElevatorAI/ElevatorAI.cs
@@ -0,0 +1,41 @@
+﻿using ElevatorSimulator.Elevator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorSimulator.ElevatorAI
+{
+    abstract class ElevatorAI : ElevatorController
+    {
+        public List<Request> PendingRequests
+        {
+            get;
+            private set;
+        }
+
+        public List<Request> PickedUpRequests
+        {
+            get;
+            private set;
+        }
+
+        public ElevatorCollection Elevators;
+
+        public Building Building;
+
+        public ElevatorAI(Building building, ElevatorCollection elevators)
+        {
+            this.Building = building;
+            this.Elevators = elevators;
+        }
+
+        public abstract void HandleRequests();
+    }
+
+    interface ElevatorController
+    {
+        void HandleRequests();
+    }
+}
